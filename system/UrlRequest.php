7faed42c817e9595703522b31cdbb999a9188f6e
Eugene Kolesnikov <ivancarevich@gmail.com>
2011-03-16 17:44:01 +0300

    Various changes

--- system/UrlRequest.php
+++ system/UrlRequest.php
@@ -1,4 +1,23 @@
 <?php
+/**
+ * 
+ * @author Eugene Kolesnikov <ivancarevich@gmail.com>
+ * @package wowAPI
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ * 
+ */
+
 namespace WOWAPI\SYSTEM;
 
 class UrlRequest
@@ -51,7 +70,7 @@ class UrlRequest
         if ( is_file($file) )
         {   
             // Cache is valid
-            if ( ($mTime + CACHE_TIME) > time() ) 
+            if ( (filemtime($file) + CACHE_TIME) > time() ) 
             {
                 return true;
             }
@@ -78,8 +97,10 @@ class UrlRequest
     private function writeCache($file, $data)
     {
         $cache      = fopen($file, 'w');
+        
         if (!fwrite($cache, $data))
             throw new \WOWAPI\EXCEPTIONS\SYSTEM\CacheFilePermissionDenied();
+        
         fclose($cache);
         return true;
     }
--- test.php
+++ test.php
@@ -2,14 +2,6 @@
 namespace WOWAPI;
 
 require __DIR__ . '/autoload.php';
+$Guild = new API\Guild( 'свежеватель-душ', 'Больничка' );
 
-$cacheUrl = 'http://eu.battle.net/wow/ru/character/%s/%s/simple';
-
-try {
-    $Page = new SYSTEM\UrlRequest( $cacheUrl );
-    $html = $Page->load('свежеватель-душ', 'Чекабой');
-} catch (\Exception $e) {
-    echo 'Message: ' .$e->getMessage();
-}
-
-var_dump($html);
+echo $Guild->name;
